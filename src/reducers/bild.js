f59b30908d5da9131f1b9933ad0d19949f973d77
Skuznetz <s.kuznetzov@mail.ru>
2018-07-12 18:57:43 +0400

    создадим массив

---
+++ src/reducers/bild.js
@@ -0,0 +1,14 @@
+export default function(){
+    return [{id:1,
+             name: 'первая',
+             desc: 'коммент 1',
+             img: 'адресс 1'},
+             {id: 2,
+              name: 'вторая',
+              desc: 'коммент 2',
+              img: 'адресс 2'},
+             {id: 3,
+              name: 'третья',
+              desc: 'коммент 3',
+              img: 'адресс 3'}]
+}
